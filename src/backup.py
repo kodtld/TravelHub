d8e7856d1772bc32f034572dadbbd12f780182a3
kodtld <kodu@live.fi>
2022-04-11 14:41:17 +0300

    major changes to main.py

--- src/backup.py
+++ src/backup.py
@@ -2,7 +2,7 @@ from tkinter import *
 import tkinter
 from tkinter import font
 import datetime
-from matplotlib.pyplot import get
+from PIL import Image, ImageTk
 
 import requests
 from requests.api import request
@@ -18,15 +18,15 @@ canvas.pack()
 class mainframe():
     def __init__(self):
         self.main_frame = Frame(root,bg="lightblue")
-        self.weather_box = Label(self.main_frame,bg="blue")
+        self.weather_box = Label(self.main_frame,bg="darkblue")
     
     def format_weather(self,got_weather):
         self.weather_box.place(relx = 0.3, rely = 0, relwidth = 0.7, relheight = 0.2)
-
-        # Current weather box
-        current_box = Label(self.weather_box,bg="blue")
-        current_box.place(relx = 0, rely = 0, relwidth = 0.2, relheight = 1)
         
+        # Current weather box -------------------------------------------------
+        current_box = Label(self.weather_box,bg="darkblue")
+        current_box.place(relx = 0, rely = 0, relwidth = 0.2, relheight = 1)
+
         current_weather_day = got_weather['daily'][0]['temp']['day']
         current_weather_min = got_weather['daily'][0]['temp']['min']
         current_date = datetime.datetime.utcfromtimestamp(got_weather['daily'][0]['dt']).strftime('%d-%m')
@@ -38,10 +38,20 @@ class mainframe():
         current_weather_day_text.place(rely=0.55,relheight=0.15,relwidth=1)
         current_weather_min_text.place(rely=0.7,relheight=0.15,relwidth=1)
         current_date_text.place(rely=0.85,relheight=0.15,relwidth=1)
-    
+
+        # Current weather icon
+        current_icon_for_call = (got_weather['daily'][0]['weather'][0]['icon'])
+        current_iconcall = f"http://openweathermap.org/img/wn/{current_icon_for_call}@2x.png"
+        current_u = urlopen(current_iconcall)
+        current_raw_data = current_u.read()
+        current_u.close()
+        current_photo = ImageTk.PhotoImage(data=current_raw_data) # <-----
+        current_icon_label = Label(current_box,image=current_photo,background="lightblue")
+        current_icon_label.image = current_photo
+        current_icon_label.place(relwidth=1,relheight=0.53)
         
-        # Tomorrow weather box
-        one_box = Label(self.weather_box,bg="blue")
+        # Tomorrow weather box -------------------------------------------------
+        one_box = Label(self.weather_box,bg="darkblue")
         one_box.place(relx = 0.2, rely = 0, relwidth = 0.2, relheight = 1)
 
         one_weather_day = got_weather['daily'][1]['temp']['day']
@@ -56,8 +66,19 @@ class mainframe():
         one_weather_min_text.place(rely=0.7,relheight=0.15,relwidth=1)
         one_date_text.place(rely=0.85,relheight=0.15,relwidth=1)
 
-        # Two day weather box
-        two_box = Label(self.weather_box,bg="blue")
+        # Tomorrow weather icon
+        one_icon_for_call = (got_weather['daily'][1]['weather'][0]['icon'])
+        one_iconcall = f"http://openweathermap.org/img/wn/{one_icon_for_call}@2x.png"
+        one_u = urlopen(one_iconcall)
+        one_raw_data = one_u.read()
+        one_u.close()
+        one_photo = ImageTk.PhotoImage(data=one_raw_data) # <-----
+        one_icon_label = Label(one_box,image=one_photo,background="lightblue")
+        one_icon_label.image = one_photo
+        one_icon_label.place(relwidth=1,relheight=0.53)
+
+        # Two day weather box -------------------------------------------------
+        two_box = Label(self.weather_box,bg="darkblue")
         two_box.place(relx = 0.4, rely = 0, relwidth = 0.2, relheight = 1)
 
         two_weather_day = got_weather['daily'][2]['temp']['day']
@@ -72,8 +93,19 @@ class mainframe():
         two_weather_min_text.place(rely=0.7,relheight=0.15,relwidth=1)
         two_date_text.place(rely=0.85,relheight=0.15,relwidth=1)
 
-        # Three day weather box
-        three_box = Label(self.weather_box,bg="blue")
+        # Two weather icon
+        two_icon_for_call = (got_weather['daily'][2]['weather'][0]['icon'])
+        two_iconcall = f"http://openweathermap.org/img/wn/{two_icon_for_call}@2x.png"
+        two_u = urlopen(two_iconcall)
+        two_raw_data = two_u.read()
+        two_u.close()
+        two_photo = ImageTk.PhotoImage(data=two_raw_data) # <-----
+        two_icon_label = Label(two_box,image=two_photo,background="lightblue")
+        two_icon_label.image = two_photo
+        two_icon_label.place(relwidth=1,relheight=0.53)
+
+        # Three day weather box -------------------------------------------------
+        three_box = Label(self.weather_box,bg="darkblue")
         three_box.place(relx = 0.6, rely = 0, relwidth = 0.2, relheight = 1)
 
         three_weather_day = got_weather['daily'][3]['temp']['day']
@@ -88,8 +120,19 @@ class mainframe():
         three_weather_min_text.place(rely=0.7,relheight=0.15,relwidth=1)
         three_date_text.place(rely=0.85,relheight=0.15,relwidth=1)
 
-        # Four day weather box
-        four_box = Label(self.weather_box,bg="blue")
+        # Three weather icon
+        three_icon_for_call = (got_weather['daily'][3]['weather'][0]['icon'])
+        three_iconcall = f"http://openweathermap.org/img/wn/{three_icon_for_call}@2x.png"
+        three_u = urlopen(three_iconcall)
+        three_raw_data = three_u.read()
+        three_u.close()
+        three_photo = ImageTk.PhotoImage(data=three_raw_data) # <-----
+        three_icon_label = Label(three_box,image=three_photo,background="lightblue")
+        three_icon_label.image = three_photo
+        three_icon_label.place(relwidth=1,relheight=0.53)
+
+        # Four day weather box -------------------------------------------------
+        four_box = Label(self.weather_box,bg="darkblue")
         four_box.place(relx = 0.8, rely = 0, relwidth = 0.2, relheight = 1)
 
         four_weather_day = got_weather['daily'][4]['temp']['day']
@@ -103,7 +146,18 @@ class mainframe():
         four_weather_day_text.place(rely=0.55,relheight=0.15,relwidth=1)
         four_weather_min_text.place(rely=0.7,relheight=0.15,relwidth=1)
         four_date_text.place(rely=0.85,relheight=0.15,relwidth=1)
-          
+
+        # Four weather icon
+        four_icon_for_call = (got_weather['daily'][4]['weather'][0]['icon'])
+        four_iconcall = f"http://openweathermap.org/img/wn/{four_icon_for_call}@2x.png"
+        four_u = urlopen(four_iconcall)
+        four_raw_data = four_u.read()
+        four_u.close()
+        four_photo = ImageTk.PhotoImage(data=four_raw_data) # <-----
+        four_icon_label = Label(four_box,image=four_photo,background="lightblue")
+        four_icon_label.image = four_photo
+        four_icon_label.place(relwidth=1,relheight=0.53)
+      
     def get_weather(self,lat,lon):
         weather_key = "10ab2060f30ce15d80acaef3490a3c36"
         weather_call = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=current,minutely,hourly,alerts&appid={weather_key}&units=metric"
@@ -114,13 +168,15 @@ class mainframe():
     def load(self,lat,lon,city,country):
         self.main_frame.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
         
-        destinationbox = Label(self.main_frame,bg="blue")
-        destinationbox.place(relx = 0, rely = 0, relwidth = 0.3, relheight = 0.15)
+        destinationbox = Label(self.main_frame,bg="darkblue")
+        destinationbox.place(relx = 0, rely = 0, relwidth = 0.3, relheight = 0.2)
 
         destinationtext = Label(destinationbox,text=f"{city}, {country}",font=("helvetica",24),bg="lightblue")
         destinationtext.place(relx=0,rely=0,relwidth=1,relheight=1)
 
         self.get_weather(lat,lon)
+
+# End of mainframe class
 
 def get_GeoCode(hp):
     
@@ -142,11 +198,6 @@ def get_GeoCode(hp):
     except:
         valid = Label(root,text="Please enter a valid destination...",font="helvetica",bg="lightblue")
         valid.place(relx = 0.3, rely = 0.75, relwidth = 0.4, relheight = 0.1)
-
-# Set Home Screen Background Image
-#bgimage = PhotoImage(file = "BGimagesmarrel.png")
-#bg_label = Label(root,image = bgimage)
-#bg_label.place(x=0,y=0,relwidth=1, relheight=1)
 
 # Home Screen Entry Box
 hp = Entry(root,bg="white",font="helvetica",justify=CENTER)
